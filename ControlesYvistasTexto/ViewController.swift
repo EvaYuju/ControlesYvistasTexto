7318b832935821f0316d053ba738622c03d7da64
Eva López <elopezmarquez@safareyes.es>
2023-04-13 12:18:16 +0200

    FINALIZADO

--- ControlesYvistasTexto/ViewController.swift
+++ ControlesYvistasTexto/ViewController.swift
@@ -19,10 +19,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var mySlider: UISlider!
     @IBOutlet weak var myStepper: UIStepper!
     @IBOutlet weak var mySwith: UISwitch!
-
     @IBOutlet weak var myActivityIndicator: UIActivityIndicatorView!
-    
     @IBOutlet weak var myProgressView: UIProgressView!
+    @IBOutlet weak var myStepperLabel: UILabel!
+    @IBOutlet weak var mySwitchLabel: UILabel!
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var myTextView: UITextView!
     
     
     // Vbles
@@ -36,14 +38,16 @@ class ViewController: UIViewController {
         
         myButton.setTitle("Mi botón", for: .normal)
         myButton.backgroundColor = .blue
-        myButton.setTitleColor(.white, for: .normal)
+        myButton.setTitleColor(.black, for: .normal)
         
         // Pickers
         
         myPickerView.backgroundColor = .lightGray
+       
         // * datasource => indicar que clase proporciona los elementos
         myPickerView.dataSource = self // self = la clase ViewController
         myPickerView.delegate = self // self = la clase ViewController
+        
         myPickerView.isHidden = true // Para que este apagado al iniciar (hasta que se active el switch
         
         // PageControls
@@ -64,11 +68,14 @@ class ViewController: UIViewController {
         
         mySlider.minimumTrackTintColor = .red   // Color
         
-        mySlider.minimumValue = 1               // Valor mínimo
+        mySlider.minimumValue = 0              // Valor mínimo
         //mySlider.maximumValue = myPickerViewValues.count -> da error hay que poner Float (Fix)
-        mySlider.maximumValue = Float(myPickerViewValues.count) // Valor max = nº valores que hay en el picker
+        mySlider.maximumValue = Float(myPickerViewValues.count) - 1 // Valor max = nº valores que hay en el picker
         
-        mySlider.value = 1                      // Valor por defecto
+        mySlider.value = 0.1                      // Valor por defecto
+        
+        mySlider.isUserInteractionEnabled = true // ???
+        
         
         // Steppers
         
@@ -84,10 +91,33 @@ class ViewController: UIViewController {
         // Progress Indicators
         
         myProgressView.progress = 0 // valor inicial 0
+        
         myActivityIndicator.startAnimating()
         myActivityIndicator.color = .magenta
         myActivityIndicator.hidesWhenStopped = true
-
+        
+        // Labels
+        myStepperLabel.textColor = .darkGray
+        myStepperLabel.font = UIFont.boldSystemFont(ofSize: 16)
+        myStepperLabel.text = "1"
+        
+        mySwitchLabel.text = "OFF"
+        
+        // TextFields
+        
+        myTextField.textColor = .magenta
+        myTextField.placeholder = "Escribe algo..."
+        
+        //myTextField.dataSource = self
+        myTextField.delegate = self
+        
+        // TextView
+        
+        myTextView.textColor = .systemPurple
+        // Para que no sea editable:
+        // myTextView.isEditable = false
+        myTextView.delegate = self
+        
     }
 
    
@@ -95,10 +125,12 @@ class ViewController: UIViewController {
     
     @IBAction func mybuttonAction(_ sender: Any) {
         if myButton.backgroundColor == .blue {
-            myButton.backgroundColor = .yellow
+            myButton.backgroundColor = .magenta
         } else {
             myButton.backgroundColor = .blue
         }
+        
+        myTextView.resignFirstResponder() // Se cierra el teclado asociado al textView
     }
     
     @IBAction func pyPageControlAction(_ sender: Any) {
@@ -116,6 +148,11 @@ class ViewController: UIViewController {
         let value = mypagecontrol.currentPage
         mySlider.value = Float(value)
         
+        // Para cambiar el valor del progress view con el valor seleccionado:
+        let selectedRow = mypagecontrol.currentPage
+        let progress = Float(selectedRow) / Float(myPickerViewValues.count - 1)
+        myProgressView.setProgress(progress, animated: true)
+        
     }
     
     @IBAction func mySegmentedControlAction(_ sender: Any) {
@@ -128,10 +165,14 @@ class ViewController: UIViewController {
         myButton.setTitle(myStringSelected, for: .normal)
         
         // Primero accedemos al valor del seg
-        //let value = myStepper.value
-        //mySlider.value = Float(value)
         let value = mySegmentedControl.selectedSegmentIndex
         mySlider.value = Float(value)
+        
+        // para la progressView
+        let selectedValue = mySegmentedControl.selectedSegmentIndex
+        let progress = Float(selectedValue) / Float(myPickerViewValues.count - 1)
+        myProgressView.setProgress(progress, animated: true)
+        
         
     }
     
@@ -141,6 +182,9 @@ class ViewController: UIViewController {
         let myStringSelected = myPickerViewValues[mypagecontrol.currentPage]
         
         var progress: Float = 0
+        
+        let value = myStepper.value
+        mySlider.value = Float(value)
         
         switch mySlider.value {
         case  1..<2:
@@ -192,7 +236,7 @@ class ViewController: UIViewController {
             
             progress = 1
             
-            //break
+            break
     
         }
         
@@ -203,6 +247,10 @@ class ViewController: UIViewController {
         // Primero accedemos al valor del stepper
         let value = myStepper.value
         mySlider.value = Float(value)
+        
+        // Interpolar
+        myStepperLabel.text = "\(value)"
+        
     }
     
     @IBAction func mySitchAction(_ sender: Any) {
@@ -210,10 +258,13 @@ class ViewController: UIViewController {
             myPickerView.isHidden = false
             myActivityIndicator.stopAnimating()
             myActivityIndicator.isHidden = true
+            mySwitchLabel.text = "ON"
         } else {
             myPickerView.isHidden = true
             myActivityIndicator.startAnimating()
             myActivityIndicator.isHidden = false
+            mySwitchLabel.text = "OFF"
+
         }
     }
     
@@ -250,10 +301,37 @@ extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
         // Para marcar la fila que tenemos seleccionada:
         mySegmentedControl.selectedSegmentIndex = row
         
-        // Slider
+        // Para cambiar el valor del slider con el valor seleccionado:
+        mySlider.value = Float(row)
 
+        // Para cambiar el valor del progress view con el valor seleccionado:
+        let progress = Float(row) / Float(myPickerViewValues.count - 1)
+        myProgressView.setProgress(progress, animated: true)
     }
-    
 
 }
 
+// Referencia al TextField
+extension ViewController: UITextFieldDelegate {
+    // Cuando pulsemos el botón return en el teclado:
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+    }
+    // cuando el textField termina su edición
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        myButton.setTitle(textField.text, for: .normal)
+    }
+}
+
+// Referencia al TextView
+extension ViewController: UITextViewDelegate {
+    // Cuando el textView comience a editarse:
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        myTextField.isHidden = true
+    }
+    // cuando finalice la edición
+    func textViewDidEndEditing(_ textView: UITextView) {
+        myTextField.isHidden = false
+
+    }
+}
